022980a76f2bf217bb119ef5adc7e21355f55f26
DeyvidA <darauz@nicarao.agency>
2024-12-06 22:58:48 -0600

    Add Technologies component and routing setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,30 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 
+import { TechnologiesComponent } from './pages/technologies/technologies.component';
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
+
+  {
+    path: 'technologies',
+    component: TechnologiesComponent,
+    children: [
+      {
+        path: 'Create',
+        loadChildren: () =>
+          import('./pages/technologies/Create/Create.module').then(
+            (m) => m.CreateModule
+          ),
+      },
+
+      //   {
+      //     path: 'edit/:id',
+      //     loadChildren: () =>
+      //       import('./pages/technologies/edit/edit.module').then(
+      //         (m) => m.EditModule
+      //       ),
+      //   },
+    ],
+  },
 ];
---
+++ src/app/pages/technologies/technologies.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-technologies',
+  imports: [],
+  templateUrl: './technologies.component.html',
+  styleUrl: './technologies.component.css',
+})
+export class TechnologiesComponent {}
