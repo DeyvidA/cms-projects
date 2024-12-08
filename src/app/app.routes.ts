cf7524a5b2e005e28e3153487295aeafe97e301e
DeyvidA <darauz@nicarao.agency>
2024-12-08 16:02:33 -0600

    Add edit functionality for technologies and projects with routing updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,31 +3,46 @@ import { LoginComponent } from './pages/login/login.component';
 import { CreateComponent } from './pages/technologies/create/create.component';
 
 import { TechnologiesComponent } from './pages/technologies/technologies.component';
+import { ListComponent } from './pages/projects/list/list.component';
+import { CreateComponent as ProjectCreateComponent } from './pages/projects/create/create.component';
+import { EditComponent } from './pages/projects/edit/edit.component';
+
 export const routes: Routes = [
   {
     path: 'technologies',
-    component: TechnologiesComponent,
     children: [
       {
-        path: 'new',
+        path: '',
+        component: TechnologiesComponent,
+      },
+      {
+        path: 'create',
         component: CreateComponent,
       },
-
-      //   {
-      //     path: 'edit/:id',
-      //     loadChildren: () =>
-      //       import('./pages/technologies/edit/edit.module').then(
-      //         (m) => m.EditModule
-      //       ),
-      //   },
+      {
+        path: 'edit/:id',
+        component: EditComponent,
+      },
     ],
   },
-
   {
-    path: 'new',
-    component: CreateComponent,
+    path: 'projects',
+    children: [
+      {
+        path: '',
+        component: ListComponent,
+      },
+      {
+        path: 'create',
+        component: ProjectCreateComponent,
+      },
+      {
+        path: 'edit/:id',
+        component: EditComponent,
+      },
+    ],
   },
-
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login', pathMatch: 'full' },
 ];
