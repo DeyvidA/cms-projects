93b7f5e2675203c1d4992124d9a2695449bd3b25
DeyvidA <darauz@nicarao.agency>
2025-03-23 13:37:36 -0600

    Remove NOT NULL constraint from demo_link and repo_link columns in projects table

---
+++ supabase/migrations/20250323193437_setRepoUrlAndDemoLinkNull.sql
@@ -0,0 +1,4 @@
+-- Remover NOT NULL de una columna específica en la tabla projects
+ALTER TABLE projects ALTER COLUMN demo_link DROP NOT NULL;
+-- Remover NOT NULL de una columna específica en la tabla projects
+ALTER TABLE projects ALTER COLUMN repo_link DROP NOT NULL;
